7f364bf53aebf80c94bece2e998f16f2928daa21
vedanshipanchal <vedanshipanchal@gmail.com>
2024-09-06 17:35:09 +0100

    datatype

---
+++ main.py
@@ -0,0 +1,18 @@
+
+# 1.string dadatype
+name = "Vedanshi" 
+age = "13" #string
+print(age)
+print(name)
+
+#2.integer datatype(numbers without points)
+age = 13
+print(age)
+
+#3. float datatype (numbers with points)
+weight = 50.5
+print(weight)
+
+#4. boolean datatype
+student = "Vedanshi"
+print(True)
